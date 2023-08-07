e884084c908c847dd156aaadc7414871bf6fa49f
TenYes <1449726675@qq.com>
2023-08-07 17:42:27 +0800

    第二次提交

--- src/main/java/com/tp/git/HelloGit.java
+++ src/main/java/com/tp/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.tp.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
